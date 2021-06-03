e73a0e675b3b626c3db7023c2b2dbf4428186828
Keshav Bansal <keshavbansal290400@gmail.com>
2021-06-03 15:18:44 +0530

    add q1 and q2
    Signed-off-by: Keshav Bansal <keshavbansal290400@gmail.com>

---
+++ lab2/q2.py
@@ -0,0 +1,11 @@
+def duplicate(l):
+    l2 = []
+    duplicates = []
+    for i in l:
+        if i in l2:
+            duplicates.append(i)
+        else:
+            l2.append(i)
+    return duplicates
+l = [1,2,3,3,4,5,6,6,7,7,45,45,56]
+print(duplicate(l))
