442734c0f0a51eab29fa4eb8d7a528ce8fd31b19
Keshav Bansal <keshavbansal290400@gmail.com>
2021-06-08 12:35:30 +0530

    fix q1 and q2
    Signed-off-by: Keshav Bansal <keshavbansal290400@gmail.com>

--- lab5/q1.py
+++ lab5/q1.py
@@ -1,14 +1,15 @@
 # Plot tan(x), cot(x), sec(x) and cosec(x) for the values of x= [-pi,-pi/4, -pi/2, 0, pi/4, pi/2, pi].
 
 import numpy as np
-X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+X = np.array([-np.pi, -np.pi/2, -np.pi/4, 0, np.pi/4, np.pi/2, np.pi])
 T, C, S, CO = np.tan(X), np.arctan(X), np.arccos(X), np.arcsin(X)
 import matplotlib.pyplot as plt
+plt.subplot(2,2,1)
 plt.plot(X, T)
-plt.show()
+plt.subplot(2,2,2)
 plt.plot(X, C)
-plt.show()
+plt.subplot(2,2,3)
 plt.plot(X, S)
-plt.show()
+plt.subplot(2,2,4)
 plt.plot(X, CO)
 plt.show()
--- lab5/q2.py
+++ lab5/q2.py
@@ -4,14 +4,14 @@
 # B 5 2
 # C 8 5
 # D 5 7
+
 import numpy as np
 import matplotlib.pyplot as plt
 n = 4
-X = np.array(['A', 'B', 'C', 'D'])
+X = np.array([1,2,3,4])
+Method = np.array(['A', 'B', 'C', 'D'])
 R1 = np.array([2, 5, 8, 5])
 R2 = np.array([3, 2, 5, 7])
-plt.subplot(1,2,1)
-plt.bar(X, R1, facecolor='#9999ff', edgecolor='white')
-plt.subplot(1,2,2)
-plt.bar(X, R2, facecolor='#ff9999', edgecolor='white')
+plt.bar(X-0.1, R1, width=0.2, facecolor='#9999ff', edgecolor='white')
+plt.bar(X+0.1, R2, width=0.2, facecolor='#ff9999', edgecolor='white')
 plt.show()
